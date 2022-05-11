7766997f478dcf2a36d5453a12e6def8fa633767
Stuart Bowes <stuartbowes2000@gmail.com>
2022-05-11 11:13:59 +0100

    Add layout for top half of task screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.blue,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'To Do',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              '12 tasks',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
