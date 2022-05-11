60fa4842605a4f594292ad89d1cc9dbf5f69df05
Stuart Bowes <stuartbowes2000@gmail.com>
2022-05-11 11:25:54 +0100

    Add layout for bottom half of task screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -6,41 +6,63 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.blue,
-              ),
-              backgroundColor: Colors.white,
-              radius: 30.0,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'To Do',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '12 tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            )
-          ],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.blue,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'To Do',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  )),
+            ),
+          )
+        ],
       ),
     );
   }
